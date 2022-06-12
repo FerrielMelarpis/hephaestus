41b1c10fa97ab79f0fb9d2fc9111e0c38e3df0db
Ferriel Melarpis <ferriel@canva.com>
2022-06-12 20:15:46 +0800

    make config module global

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,9 +1,15 @@
+import * as path from 'path';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { NextModule } from './next/next.module';
 import { ServerModule } from './server.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: path.join(__dirname, '/../', `.env.${process.env.NODE_ENV}`),
+      isGlobal: true,
+    }),
     // APIs
     ServerModule,
     // Client-side
