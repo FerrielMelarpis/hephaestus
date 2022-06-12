6884c270eda695351c52f49716cf6a6cfb99592a
Ferriel Melarpis <ferriel@canva.com>
2022-06-12 20:13:55 +0800

    updated bot_card

--- src/client/components/bot_card/bot_card.tsx
+++ src/client/components/bot_card/bot_card.tsx
@@ -2,27 +2,30 @@ import React from 'react';
 import { Box, Image, Text } from '@chakra-ui/react';
 import type { Bot } from 'shared/types/bots';
 import { getAvatarUrl } from 'shared/utils';
+import Link from 'next/link';
 
-type BotCardProps = Omit<Bot, 'id'> & {
-  // todo: link cards to specific view for editing and deletion
-  // url: string;
-};
-export const BotCard = ({ name, purpose }: BotCardProps) => {
+export const BotCard = ({ id, name, purpose }: Bot) => {
   return (
     <Box
       maxW="sm"
-      borderWidth="1px"
       borderRadius="lg"
       overflow="hidden"
       paddingY={4}
       paddingX={6}
       boxShadow="outline"
     >
-      <Image src={getAvatarUrl(name)} alt={`Avatar for bot named ${name}`} />
-      <Box marginTop={4}>
-        <Text fontSize="lg">{name}</Text>
-        <Text fontSize="md">{purpose}</Text>
-      </Box>
+      <Link key={id} href="/bots/[id]" as={`/bots/${id}`}>
+        <a>
+          <Image
+            src={getAvatarUrl(name)}
+            alt={`Avatar for bot named ${name}`}
+          />
+          <Box marginTop={4}>
+            <Text fontSize="xl">{name}</Text>
+            <Text fontSize="md">{purpose}</Text>
+          </Box>
+        </a>
+      </Link>
     </Box>
   );
 };
